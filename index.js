8e431e2ee201a4724919f3323b62e7341d1e518f
Mike Shippy <mikeship04@gmail.com>
2022-06-08 10:56:41 -0700

    commit tree 329496ee9b880dfe9f3b3f1fd5c5792f586048da

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solutions in this file
+const gifts = ['teddy bear', 'drone', 'doll'];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`)
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+function writeCards(namesArray, event) {
+    let thankYouCards = []
+    for(let i = 0; i < namesArray.length; i++) {
+        thankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards
+}
+
+// takes in a positive integer
+// starting with that number counts down to 0
+ 
+function countDown(posInt) {
+    while(posInt > 0) {
+        console.log(posInt);
+    posInt -= 1;
+    };
+    console.log(posInt);
+};
